993966ea913345f51d63ff04a71458dc5f3d7d9c
yongketian@gmail.com <tyk>
2024-08-09 00:20:07 +0800

    output

--- src/transformers_demo/chapter1/lesson3.py
+++ src/transformers_demo/chapter1/lesson3.py
@@ -4,3 +4,4 @@ classifier = pipeline("sentiment-analysis", device=0)
 output = classifier("I've been waiting for a HuggingFace course my whole life.")
 
 print(output)
+# [{'label': 'POSITIVE', 'score': 0.9598048329353333}]
