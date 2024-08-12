99f1590af55a746af8236f80f9d3a4ed266e9b92
yongketian@gmail.com <tyk>
2024-08-12 23:29:33 +0800

    parse chatml

--- src/chapter1/lesson3/roleplay_task.py
+++ src/chapter1/lesson3/roleplay_task.py
@@ -1,28 +1,40 @@
 import torch
+import re
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# 上下文对话
-messages = []
+from typing import Tuple
 
-def load_model():
+def parse_chatml(chatml_content):
+    pattern = re.compile(r'<\|im_start\|>(?:\s+)([a-zA-Z]+)(?:\s+)(.*?)<\|im_end\|>', re.DOTALL)
+    messages = []
+    for match in pattern.finditer(chatml_content):
+        role, content = match.groups()
+        messages.append({"role": role, "content": content.strip()})
+    return messages
+
+def load_model() -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
     model_name_or_path = "ClosedCharacter/Peach-9B-8k-Roleplay"
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
     model = AutoModelForCausalLM.from_pretrained(
         model_name_or_path, torch_dtype=torch.bfloat16, 
-        trust_remote_code=True, device="mps")
+        trust_remote_code=True, device_map="auto")
     return model, tokenizer
 
 def main():
     print("欢迎来到角色扮演游戏！")
     print("模型加载中...")
     model, tokenizer = load_model()
-    role = input("请输入角色：")
+    role = input("请输入角色：").strip()
+
+    # 上下文对话
+    messages = []
     messages.append({"role": "system", "content": "你是%s" % role})
-    messages.append({"role": "user", "content": "你好，你是谁"})
+    messages.append({"role": "use", "content": "你好，你是谁？"})
 
     try: 
+        print("游戏开始！")
         while True: 
-            input_ids = tokenizer.apply_chat_template(conversation=messages, tokenize=True, return_tensors="pt")
+            input_ids = tokenizer.apply_chat_template(conversation=messages, tokenize=True, return_tensors="pt",)
             output = model.generate(
                 inputs=input_ids.to("mps"), 
                 temperature=0.3, 
@@ -31,13 +43,12 @@ def main():
                 repetition_penalty=1.1,
                 max_new_tokens=512)
             response = tokenizer.decode(output[0])
-            print(response)
 
-            # 保存用户输入和模型回复
-            messages.append({"role": "system", "content": response})
+            messages = parse_chatml(response)
+            print("输出：%s" % messages[-1]['content'])
 
             # 用户输入
-            message = input("请输入消息：")
+            message = input("请输入消息：").strip()
             messages.append({"role": "user", "content": message})
     except KeyboardInterrupt:
         print("游戏结束！")
